c4e7aa6be538ba468fee1e87ec36391c8f54cb0e
nheid <mail@nicoheid.com>
2011-02-06 12:00:19 +0100

    merged brunoqc's pull request

--- src/com/unitedcoders/gpodder/GpodderSubscriptions.java
+++ src/com/unitedcoders/gpodder/GpodderSubscriptions.java
@@ -2,55 +2,62 @@ package com.unitedcoders.gpodder;
 
 public class GpodderSubscriptions {
 
-	private String title;
-	private String url;
-	private String description;
-	private String subscribers;
-	private String logo_url;
-	private String website;
-	private String mygpo_link;
-	
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getSubscribers() {
-		return subscribers;
-	}
-	public void setSubscribers(String subscribers) {
-		this.subscribers = subscribers;
-	}
-	public String getLogo_url() {
-		return logo_url;
-	}
-	public void setLogo_url(String logoUrl) {
-		logo_url = logoUrl;
-	}
-	public String getWebsite() {
-		return website;
-	}
-	public void setWebsite(String website) {
-		this.website = website;
-	}
-	public String getMygpo_link() {
-		return mygpo_link;
-	}
-	public void setMygpo_link(String mygpoLink) {
-		mygpo_link = mygpoLink;
-	}
+    private String title;
+    private String url;
+    private String description;
+    private String subscribers;
+    private String subscribers_last_week;
+    private String logo_url;
+    private String website;
+    private String mygpo_link;
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getSubscribers() {
+        return subscribers;
+    }
+    public void setSubscribers(String subscribers) {
+        this.subscribers = subscribers;
+    }
+    public String getSubscribers_last_week() {
+        return subscribers_last_week;
+    }
+    public void setSubscribers_last_week(String subscribers_last_week) {
+        this.subscribers_last_week = subscribers_last_week;
+    }
+    public String getLogo_url() {
+        return logo_url;
+    }
+    public void setLogo_url(String logoUrl) {
+        logo_url = logoUrl;
+    }
+    public String getWebsite() {
+        return website;
+    }
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+    public String getMygpo_link() {
+        return mygpo_link;
+    }
+    public void setMygpo_link(String mygpoLink) {
+        mygpo_link = mygpoLink;
+    }
 
 }
