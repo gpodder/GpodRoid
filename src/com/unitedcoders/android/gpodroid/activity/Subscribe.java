5291bd044dbcad6d9348bfc9a4ad610fdee9a4a9
Nico Heid <mail@nicoheid.com>
2011-05-01 17:48:35 +0200

    adding User-Agent info of the WebView we are using

--- src/com/unitedcoders/android/gpodroid/activity/Subscribe.java
+++ src/com/unitedcoders/android/gpodroid/activity/Subscribe.java
@@ -1,7 +1,10 @@
 package com.unitedcoders.android.gpodroid.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -11,19 +14,25 @@ import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import com.unitedcoders.android.gpodroid.GpodRoid;
 import com.unitedcoders.android.gpodroid.R;
 import com.unitedcoders.android.gpodroid.services.UpdateService;
 import com.unitedcoders.gpodder.GpodderAPI;
-import roboguice.activity.RoboListActivity;
+import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Subscribe extends RoboListActivity implements View.OnClickListener {
+public class Subscribe extends RoboActivity implements View.OnClickListener {
 
-    String subscribeTo;
+    Context context;
+    @InjectView(R.id.lv_search_results) ListView lvSearchResults;
+    ProgressDialog searching;
+
+
+    private final Handler handler = new Handler();
     ArrayList<String> top25;
     HashMap<String, String> top25hm;
 
@@ -32,15 +41,15 @@ public class Subscribe extends RoboListActivity implements View.OnClickListener 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.context = getApplicationContext();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subscribe);
 
-        top25hm = GpodderAPI.getTopSubscriptions();
-        top25 = new ArrayList<String>(top25hm.keySet());
-        this.setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, top25));
-        registerForContextMenu(getListView());
+        searching = ProgressDialog.show(this, "Searching ...", "digging through podcasts", true, false);
+        displayTopPodcasts();
 
         btnPodcastSearch.setOnClickListener(this);
+        registerForContextMenu(lvSearchResults);
 
     }
 
@@ -54,7 +63,7 @@ public class Subscribe extends RoboListActivity implements View.OnClickListener 
     public boolean onContextItemSelected(MenuItem item) {
 
         AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-        final String feed = (String) getListView().getItemAtPosition(info.position);
+        final String feed = (String) lvSearchResults.getItemAtPosition(info.position);
         Log.d(GpodRoid.LOGTAG, "subscribing to " + top25hm.get(feed));
 
         new Thread(new Runnable() {
@@ -74,10 +83,45 @@ public class Subscribe extends RoboListActivity implements View.OnClickListener 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_podcast_search:
-                top25hm = GpodderAPI.searchFeeds("search");
-                top25 = new ArrayList<String>(top25hm.keySet());
-                this.setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, top25));
+                searching = ProgressDialog.show(this, "Searching ...", "digging through podcasts", true, false);
+                searchPodcasts(etPodcastSearch.getText().toString());
                 break;
         }
+    }
+
+
+    private void searchPodcasts(final String searchTerm) {
+        Thread t = new Thread() {
+            public void run() {
+                top25hm = GpodderAPI.searchFeeds(searchTerm);
+                top25 = new ArrayList<String>(top25hm.keySet());
+                handler.post(displayResults);
+            }
+        };
+        t.start();
+    }
+
+    private void displayTopPodcasts() {
+        Thread t = new Thread() {
+            public void run() {
+                top25hm = GpodderAPI.getTopSubscriptions();
+                top25 = new ArrayList<String>(top25hm.keySet());
+                handler.post(displayResults);
+            }
+        };
+        t.start();
+
+    }
+
+    final Runnable displayResults = new Runnable() {
+        @Override
+        public void run() {
+            displayResultsinUI();
+        }
+    };
+
+    private void displayResultsinUI() {
+        lvSearchResults.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, top25));
+        searching.dismiss();
     }
 }
