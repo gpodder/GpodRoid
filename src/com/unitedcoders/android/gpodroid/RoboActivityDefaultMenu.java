480b9f5fb81d807febdf73f772c7174a6d6e72a6
Nico Heid <nheid@harpy.(none)>
2011-09-19 21:32:33 +0200

    sending feedback email via menu

--- src/com/unitedcoders/android/gpodroid/RoboActivityDefaultMenu.java
+++ src/com/unitedcoders/android/gpodroid/RoboActivityDefaultMenu.java
@@ -1,6 +1,9 @@
 package com.unitedcoders.android.gpodroid;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -9,11 +12,9 @@ import com.unitedcoders.android.gpodroid.services.UpdateService;
 import roboguice.activity.RoboActivity;
 
 /**
- * Created by IntelliJ IDEA.
- * User: nheid
- * Date: 4/2/11
- * Time: 1:37 PM
- * To change this template use File | Settings | File Templates.
+ * Activity with default menu
+ *
+ * @author nheid
  */
 public class RoboActivityDefaultMenu extends RoboActivity {
 
@@ -38,9 +39,42 @@ public class RoboActivityDefaultMenu extends RoboActivity {
             case R.id.fetch_updates:
                 startService(new Intent(getApplicationContext(), UpdateService.class));
                 return true;
+            case R.id.send_feedback:
+                sendFeedback();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+
+    /**
+     * Start intend to send a feedback email via installed mail application
+     */
+    private void sendFeedback() {
+        String version = null;
+
+        try {
+            version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(GpodRoid.LOGTAG, "could not determin version", e);
+        }
+
+        String message = String.format("\n\n--DEVELOPER INFO -- \nthis feedback is from version: %s on device: %s with android: %s",
+                version , Build.MODEL, Build.VERSION.RELEASE);
+
+
+        /* Create the Intent */
+        final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+
+        /* Fill it with Data */
+        emailIntent.setType("plain/text");
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"gpodroid@united-coders.com"});
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "feedback from GPodRoid");
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, message);
+
+        /* Send it off to the Activity-Chooser */
+        GpodRoid.context.startActivity(Intent.createChooser(emailIntent, "Send mail...")
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
 }
